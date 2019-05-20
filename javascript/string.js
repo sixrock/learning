439a337e4ea741787d2911ac4397a9401ecd9b16
ziyouzizai <ziyouzizai@publicknow.com>
2019-05-20 14:56:40 +0800

    move array example to javascript dir

--- javascript/string.js
+++ javascript/string.js
@@ -1,9 +1,4 @@
-function separate (string) {
-var n;
- for (var i = 0; i < string.length; i++){
-  var[i] = steing[i];
- }
-return n;
+function jion() {
+
 }
-var string = "你好啊";
-console.log(separate(string));
+jion();
